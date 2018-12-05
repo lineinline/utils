43cc9112125c196bff98f2217201e44540a0a09d
YAJUENET\gzm210 <gzlaiyongqiang@corp.netease.com>
2018-12-05 18:59:38 +0800

    fix:导出getRandomInt函数

--- src/tool.js
+++ src/tool.js
@@ -56,7 +56,7 @@ export function toThousands(value) {
  * @param {[Number]} max 最大值 
  * @return {[Number]} 返回往下取整的随机数
  */
-function getRandomInt(min, max) {
+export function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
